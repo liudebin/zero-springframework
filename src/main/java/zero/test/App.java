9e2039da545c29ebee33afc2a9fcf06d97b1ff74
liuguobin <liuguobin@nonobank.com>
2018-07-17 10:48:05 +0800

    准备精简

---
+++ src/main/java/zero/test/App.java
@@ -0,0 +1,12 @@
+package zero.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public  void sayHi() {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/zero/test/ApplicationContextTest.java
@@ -0,0 +1,23 @@
+package zero.test;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * ApplicationContext的测试用例
+ *
+ * @author liuguobin
+ * @date 2017/10/16
+ */
+@Deprecated
+public class ApplicationContextTest extends AbstractTest {
+
+    @Test
+    public void testLoad() {
+        ApplicationContext context=new ClassPathXmlApplicationContext("applicationContext.xml");
+//        ApplicationContext context1=new FileSystemXmlApplicationContext("classpath:applicationContext.xml");
+        App app = (App) context.getBean("app");
+        app.sayHi();
+    }
+}
